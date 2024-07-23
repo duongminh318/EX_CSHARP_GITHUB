d5bf40bb9afc1a0129e056c3d56941db3df2da08
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-07-23 15:49:39 +0700

    ok

---
+++ EX1/demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace EX1
+{
+    internal class demo
+    {
+        public int MyProperty { get; set; }
+        public string MyPhone { get; set; }
+    }
+}
