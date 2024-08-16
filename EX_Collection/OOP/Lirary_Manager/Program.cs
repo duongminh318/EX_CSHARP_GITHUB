f5314841275970af62006b2c4307ebc7c3486141
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-08-16 22:03:34 +0700

    created class author

--- EX_Collection/OOP/Lirary_Manager/Program.cs
+++ EX_Collection/OOP/Lirary_Manager/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+/*
+ * Bài tập 1: Quản lý thư viện
+    Xây dựng một hệ thống quản lý thư viện đơn giản với các lớp Book, 
+    Author, và Library. Mỗi Book có thuộc tính như Title, Author, ISBN, 
+    và PublishedYear. Mỗi Author có các thuộc tính như Name, DateOfBirth, 
+    và danh sách các Book mà họ đã viết.
+
+Yêu cầu:
+- Thêm, xóa, tìm kiếm sách trong thư viện.
+- Tìm kiếm sách theo tên tác giả.
+- In ra danh sách các cuốn sách theo thứ tự năm xuất bản.
+
+
+ */
